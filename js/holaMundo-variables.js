cee375c8cbeb1a342c56d37255ab88bc40d01f4e
yulianaM <yulianamallorga@gmail.com>
2022-09-29 20:28:58 -0300

    practica de js

---
+++ js/holaMundo-variables.js
@@ -0,0 +1,50 @@
+console.log("HOLA MUNDO");
+
+//comentario de una sola linea
+console.log("despues del comentario");
+/*
+comentaro de varias lineas
+ aqui sigue el comentario*/
+ console.log(`comentario con comillas simples`)
+document.write("escribir cosas en  el navegador")
+//definicion de variables o asignaciòn de variables
+let numero = 5; //el signo de = es el operador de asignacion de valor
+console.log("numero")
+
+let numero2 = "8";
+
+console.log(numero2)
+
+numero2 = numero // reasigno un valor a una variable
+
+console.log(numero2)
+
+const palabra = "hola" //las constantes son inmutables, no se pueden reasignar valor
+
+console.log (palabra)
+//palabra = "chau"
+
+var numero3 = 10;
+
+let variable = "valor" 
+//tipo de dato string
+//defino un string vacio
+let stringVacio ="";
+
+let numberString = "8"
+
+let numberString2 = "18"
+
+console.log(numberString)
+console.log(numberString)
+document.write(numberString + numberString2); //conactenar es unir
+//tipo de numero
+let num = 10;
+let number = 20;
+console.log(num + number)
+document.write(number+num);
+//tipo de dato buleano
+
+let verdadero = true;
+let falso = false;
+console.log(!verdadero)//negacion con el operador.
